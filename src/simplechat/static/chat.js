c957450467f2642905235c21082a9cad1ae82b6b
muzak23 <logan.nommensen@gmail.com>
2023-04-17 03:21:21 -0400

    Facelift, not finished fully

--- src/simplechat/static/chat.js
+++ src/simplechat/static/chat.js
@@ -1,5 +1,5 @@
 function onDOMContentLoaded(evt) {
-    domStream = document.getElementById('messages');
+    chatStream = document.getElementById('messages');
     document.getElementById('messageForm').addEventListener('submit', onMessageSubmit, false);
 
     showHistory();
@@ -8,32 +8,56 @@ document.addEventListener('DOMContentLoaded', onDOMContentLoaded, false);
 
 
 
-function createMessageDOM(msg) {
+function createMessageHTML(msg) {
+    //     <div className="d-flex flex-row justify-content-start">
+    //       <img src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
+    //         alt="avatar 1" style="width: 45px; height: 100%;" class="rounded">
+    //       <div>
+    //         <p class="small p-2 ms-3 mb-1 rounded-3" style="background-color: #d7d7d7;">Hi</p>
+    //         <p class="small ms-3 mb-3 rounded-3 text-muted">23:58</p>
+    //         </div>
+    //     </div>
+
     let html = document.createElement('div');
-    html.className = 'message';
+    html.className = 'd-flex flex-row justify-content-start';
 
     let img = document.createElement('img');
     img.src = 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50';
     img.alt = 'User Avatar';
+    img.style = 'width: 45px; height: 100%;';
+    img.className = 'rounded align-self-center';
     html.appendChild(img);
 
-    let h3 = document.createElement('h3');
-    h3.className = 'message-author';
-    debug = msg['username']
-    h3.textContent = debug
+    let div = document.createElement('div');
 
-    html.appendChild(h3);
+    let p0 = document.createElement('p');
+    p0.className = 'small ms-3 mb-0 rounded-3 text-muted';
+    p0.textContent = msg['username'];
+    div.appendChild(p0);
 
-    let p = document.createElement('p');
-    p.className = 'message_text';
-    p.textContent = msg['text'];
-    html.appendChild(p);
+    let div2 = document.createElement('div');
+    div2.className = 'd-inline-flex flex-row';
+
+    let p1 = document.createElement('p');
+    p1.className = 'small p-2 ms-3 mb-1 rounded-3 text-wrap';
+    p1.style = 'background-color: #d7d7d7;';
+    p1.textContent = msg['text'];
+    div2.appendChild(p1);
+
+    div.appendChild(div2)
+
+    // let p2 = document.createElement('p');
+    // p2.className = 'small ms-3 mb-3 rounded-3 text-muted';
+    // p2.textContent = msg['time'];
+    // div.appendChild(p2);
+
+    html.appendChild(div);
 
     return html;
 }
 
 function showMsg(msg) {
-    domStream.insertBefore(createMessageDOM(msg), domStream.firstChild);
+    document.getElementById('messages').appendChild(createMessageHTML(msg));
 }
 
 const socket = io();
@@ -47,7 +71,7 @@ socket.on('messageReceived', function() {
 });
 
 socket.on('newMessage', function(data) {
-    document.getElementById('messages').appendChild(createMessageDOM(data));
+    showMsg(data);
 });
 
 function sendMessage(message) {
@@ -65,7 +89,7 @@ function onMessageSubmit(evt) {
 function showHistory() {
     socket.emit('getHistory', function (data) {
         for (const msg of data) {
-            showMsg(msg)
+            chatStream.insertBefore(createMessageHTML(msg), chatStream.firstChild);
     }})
 }
 
